c61ccf6ab4ea3beb02343e0bebd6fad09d9bfb51
Omelchenko Natalia <natom654@gmail.com>
2023-05-18 22:50:50 +0300

    commit second

--- src/main/java/CipherEN.java
+++ src/main/java/CipherEN.java
@@ -1,23 +1,23 @@
 import java.util.Arrays;
 import java.util.List;
 
-public class Cipher {
-    String alphabetUp = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ";
-    String alphabetLit = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя";
+public class CipherEN {
+    String alphabetUpEN = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    String alphabetLitEN = "abcdefghijklmnopqrstuvwxyz";
 
-    public String encryptText(String text, int key) {
+    public String encryptTextEn(String text, int key) {
         String encryptText = "";
         for (int i = 0; i < text.length(); i++)
             if (Character.isLetter(text.charAt(i))) {
                 if (Character.isUpperCase(text.charAt(i))) {
-                    int charPosition = alphabetUp.indexOf(text.charAt(i));
-                    int keyValue = (key + charPosition) % alphabetUp.length();
-                    char replace = alphabetUp.charAt(keyValue);
+                    int charPosition = alphabetUpEN.indexOf(text.charAt(i));
+                    int keyValue = (key + charPosition) % alphabetUpEN.length();
+                    char replace = alphabetUpEN.charAt(keyValue);
                     encryptText += replace;
                 } else if (Character.isLowerCase(text.charAt(i))) {
-                    int charPosition = alphabetLit.indexOf(text.charAt(i));
-                    int keyValue = (key + charPosition) % alphabetLit.length();
-                    char replace = alphabetLit.charAt(keyValue);
+                    int charPosition = alphabetLitEN.indexOf(text.charAt(i));
+                    int keyValue = (key + charPosition) % alphabetLitEN.length();
+                    char replace = alphabetLitEN.charAt(keyValue);
                     encryptText += replace;
                 }
             } else {
@@ -27,35 +27,35 @@ public class Cipher {
         return encryptText;
     }
 
-    public String decryptText(String text, int key) {
-        return encryptText(text, 33 - (key % 33));
+    public String decryptTextEn(String text, int key) {
+        return encryptTextEn(text, 26 - (key % 26));
     }
 
-    public String bruteForceText(String text) {
+    public String bruteForceTextEn(String text) {
         int key;
         String textUp = text.toUpperCase();
         char newChar;
         StringBuilder sbdecrypt = new StringBuilder(textUp);
 
-        for (key = 1; key < 33; key++) {
+        for (key = 1; key < 27; key++) {
 
             sbdecrypt = new StringBuilder(textUp);
             for (int i = 0; i < sbdecrypt.length(); i++) {
 
                 char currentchar = sbdecrypt.charAt(i);
 
-                int index = alphabetUp.indexOf(currentchar);
+                int index = alphabetUpEN.indexOf(currentchar);
 
                 if (index != -1) {
                     index = index - key;
 
                     if (index < 0) {
-                        index = index + 33;
+                        index = index + 26;
 
-                        newChar = alphabetUp.charAt(index);
+                        newChar = alphabetUpEN.charAt(index);
                         sbdecrypt.setCharAt(i, newChar);
                     } else {
-                        newChar = alphabetUp.charAt(index);
+                        newChar = alphabetUpEN.charAt(index);
                         sbdecrypt.setCharAt(i, newChar);
                     }
                 }
--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -3,18 +3,27 @@ import java.util.Scanner;
 
 public class Console {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        FileApplication fileApplication = new FileApplication();
+        Cipher cipher = new Cipher();
+        CipherEN cipherEN = new CipherEN();
+        String text;
 
         Scanner console = new Scanner(System.in);
+        System.out.println(" ");
         System.out.println("Choose what do you want to do with the text?");
         System.out.println("Write  e - for ENCRYPT, d - for DECRYPT, b - for BRUTE_FORCE");
         String command = console.nextLine();
 
         System.out.println("Write path to the file");
         String path = console.nextLine();
-        String text;
-        FileApplication fileApplication = new FileApplication();
-        Cipher cipher = new Cipher();
+
+        System.out.println("What is text language?");
+        System.out.println("Write  ua - for UKRAINIAN,en - for ENGLISH");
+
+        String language = console.nextLine();
+
 
         try {
             text = fileApplication.readFile(path);
@@ -25,17 +34,30 @@ public class Console {
                 String pathForEncryptCipher = fileApplication.giveNewFileName(path, Command.ENCRYPTED);
 
                 try {
-                    fileApplication.writeFile(pathForEncryptCipher, cipher.encryptText(text, key));
+                    if (language.equalsIgnoreCase("UA")) {
+                        fileApplication.writeFile(pathForEncryptCipher, cipher.encryptText(text, key));
+
+                    } else if (language.equalsIgnoreCase("EN")) {
+                        fileApplication.writeFile(pathForEncryptCipher, cipherEN.encryptTextEn(text, key));
+                    }
                     System.out.println("Successfully written Message to the file");
                 } catch (IOException e) {
                     System.out.println("There is no file to write");
                 }
+
             } else if (command.equalsIgnoreCase("D")) {
                 System.out.println("Write key for cipher");
                 int key = console.nextInt();
+
                 String pathForDecryptCipher = fileApplication.giveNewFileName(path, Command.DECRYPTED);
+
                 try {
-                    fileApplication.writeFile(pathForDecryptCipher, cipher.decryptText(text, key));
+                    if (language.equalsIgnoreCase("UA")) {
+                        fileApplication.writeFile(pathForDecryptCipher, cipher.decryptText(text, key));
+
+                    } else if (language.equalsIgnoreCase("EN")) {
+                        fileApplication.writeFile(pathForDecryptCipher, cipherEN.decryptTextEn(text, key));
+                    }
                     System.out.println("Successfully written Message to the file");
                 } catch (IOException e) {
                     System.out.println("There is no file to write");
@@ -44,19 +66,21 @@ public class Console {
 
                 String pathForBruteForceCipher = fileApplication.giveNewFileName(path, Command.BRUTE_FORCE);
                 try {
-                    fileApplication.writeFile(pathForBruteForceCipher, cipher.bruteForceText(text));
+                    if (language.equalsIgnoreCase("UA")) {
+                        fileApplication.writeFile(pathForBruteForceCipher, cipher.bruteForceText(text));
+                    } else if (language.equalsIgnoreCase("EN")) {
+                        fileApplication.writeFile(pathForBruteForceCipher, cipherEN.bruteForceTextEn(text));
+                    }
                     System.out.println("Successfully written Message to the file");
                 } catch (IOException e) {
                     System.out.println("There is no file to write");
                 }
 
             } else {
-                System.out.println("Please, make your сhoice");
+                System.out.println(" your сhoice is wrong");
             }
         } catch (IOException e) {
             System.out.println("There is no file on this path");
-
-
         }
         console.close();
     }
